5075c8d5573fd5e34b74f42f7a5fc224a624fc72
Laimonas Masionis <71811684+LaimonasMas@users.noreply.github.com>
2021-04-15 15:22:38 +0300

    small fix

--- resources/views/restaurant/index.blade.php
+++ resources/views/restaurant/index.blade.php
@@ -27,35 +27,36 @@
                         <a href="{{route('restaurant.index')}}" class="btn btn-outline-secondary btn-sm">Clear</a>
                     </div>
                     {{-- filtravimo ppabaiga --}}
+                </div>
 
 
 
 
-                    <div class="card-body">
+                <div class="card-body">
 
-                        @foreach ($restaurants as $restaurant)
-                        <li class="list-group-item list-line">
-                            <div>
-                                <h4>Restaurant: {{$restaurant->title}}</h4>
-                                <h5>Customers: {{$restaurant->customers}}</h5>
-                                <h5>Employees: {{$restaurant->employees}}</h5>
-                                <h5>Dish: {{$restaurant->restaurantMenu->title}}</h5>
+                    @foreach ($restaurants as $restaurant)
+                    <li class="list-group-item list-line">
+                        <div>
+                            <h4>Restaurant: {{$restaurant->title}}</h4>
+                            <h5>Customers: {{$restaurant->customers}}</h5>
+                            <h5>Employees: {{$restaurant->employees}}</h5>
+                            <h5>Dish: {{$restaurant->restaurantMenu->title}}</h5>
+                        </div>
+                        <div class="list-line__buttons">
+                            <div class="form-group">
+                                <a class="btn btn-outline-secondary btn-sm" href="{{route('restaurant.edit',[$restaurant])}}">EDIT</a>
                             </div>
-                            <div class="list-line__buttons">
-                                <div class="form-group">
-                                    <a class="btn btn-outline-secondary btn-sm" href="{{route('restaurant.edit',[$restaurant])}}">EDIT</a>
-                                </div>
-                                <form method="POST" action="{{route('restaurant.destroy', [$restaurant])}}">
-                                    @csrf
-                                    <button type="submit" class="btn btn-outline-danger btn-sm">DELETE</button>
-                                </form>
-                            </div>
-                        </li>
-                        @endforeach
+                            <form method="POST" action="{{route('restaurant.destroy', [$restaurant])}}">
+                                @csrf
+                                <button type="submit" class="btn btn-outline-danger btn-sm">DELETE</button>
+                            </form>
+                        </div>
+                    </li>
+                    @endforeach
 
-                    </div>
                 </div>
             </div>
         </div>
     </div>
-    @endsection
+</div>
+@endsection
